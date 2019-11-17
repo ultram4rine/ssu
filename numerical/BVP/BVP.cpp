deafca6dbf19c3388f966cceae291f6903d1901c
ultram4rine <ultramarine730@gmail.com>
2019-11-17 22:59:29 +0400

    Finally fixed

--- numerical/BVP/BVP.cpp
+++ numerical/BVP/BVP.cpp
@@ -1,11 +1,13 @@
 ﻿#define _USE_MATH_DEFINES
 
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <cmath>
 #include <complex>
 #include <tuple>
 
+typedef std::complex<double> complex;
 typedef std::vector<double> vector;
 typedef std::vector<std::complex<double>> cvector;
 
@@ -18,72 +20,107 @@ std::complex<double> y(double x)
 	return -1.0 * pow(M_E, (-1. - 1i) * x);
 }
 
-std::tuple<vector, cvector> matrix_init(int N, double h)
+cvector f_init(int N, int n)
+{
+	cvector f(n, 0);
+
+	f.at(0) = y(0);
+	f.at(n - 1) = y(N);
+
+	return f;
+}
+
+std::tuple<vector, cvector, vector> matrix_init(int N, double h)
 {
 	using namespace std::complex_literals;
 
-	vector a(N - 1, 1); cvector b(N, -2. - h * h * 2i);
+	vector a(N / h, 1); cvector c(N / h, -2. - h * h * 2i); vector b(N / h, 1);
 
-	return std::make_tuple(a, b);
+	a.at(0) = 0;
+	b.at(N / h - 1) = 0;
+
+	return std::make_tuple(a, c, b);
 }
 
-cvector NewtonMethod(int grid_start, int grid_end, int N, double h)
+cvector ThomasAlg(int N, double h)
 {
-	vector x(N + 2);
+	complex m;
 
-	x.at(0) = grid_start;
+	vector a, b; cvector c;
 
-	for (auto i = x.begin() + 1; i < x.end(); i++)
-		*i = *(i - 1) + h;
+	std::tie(a, c, b) = matrix_init(N, h);
 
-	cvector u(N), b; vector a;
+	int n = N / h;
 
-	std::tie(a, b) = matrix_init(N, h);
+	cvector f = f_init(N, n);
 
-	cvector alpha(N), beta(N);
-
-	alpha.at(1) = a.at(0) / b.at(0);
-	beta.at(1) = y(x.at(0)) / b.at(0);
-
-	for (auto i = 1; i < N - 1; i++)
+	for (int i = 1; i < n; i++)
 	{
-		alpha.at(i + 1) = a.at(i - 1) / (b.at(i) - a.at(i - 1) * alpha.at(i));
-		beta.at(i + 1) = (0. + a.at(i - 1) * beta.at(i)) / (b.at(i) - a.at(i - 1) * alpha.at(i));
+		m = a.at(i) / c.at(i - 1);
+		c.at(i) = c.at(i) - m * b.at(i - 1);
+		f.at(i) = f.at(i) - m * f.at(i - 1);
 	}
 
-	u.at(N - 1) = (y(x.at(N + 1)) + a.at(N - 2) * beta.at(N - 1)) / (b.at(N - 1) - a.at(N - 2) * alpha.at(N - 1));
+	cvector u(n);
 
-	for (auto i = N - 2; i > -1; i--)
-		u.at(i) = alpha.at(i + 1) * u.at(i + 1) + beta.at(i + 1);
+	u.at(n - 1) = f.at(n - 1) / c.at(n - 1);
+
+	for (int i = n - 2; i >= 0; i--)
+	{
+		u.at(i) = (f.at(i) - b.at(i) * u.at(i + 1)) / c.at(i);
+	}
 
 	return u;
 }
 
 int main()
 {
-	long int grid_start = 0, grid_end = 1, N = 3;
-	double h = 1. / 4.;
+	int k = 0;
+
+	long int N = 1;
+	double h = 1. / 4., error, prev_error;
 
 	std::string goon = "y";
 
 	while (goon == "y")
 	{
-		cvector u = NewtonMethod(grid_start, grid_end, N, h);
+		cvector u = ThomasAlg(N, h);
+
+		/*for (auto local_h = h; local_h <= N; local_h += h)
+			std::cout << y(local_h) << '\n';
+
+		std::cout << '\n';
+
 		for (auto i = u.begin(); i < u.end(); i++)
 			std::cout << *i << '\n';
 
+		std::cout << '\n';*/
+
 		double local_h = h;
+
+		error = abs(y(local_h)) - abs(u.at(0));
 
 		for (auto i = u.begin(); i < u.end(); i++)
 		{
-			std::cout << "diff: " << y(local_h) - *i << '\n';
+			abs(y(local_h)) - abs(*i) > error ? error = abs(y(local_h)) - abs(*i) : error;
+
+			//std::cout << "diff at " << h << ": " << abs(y(local_h)) - abs(*i) << '\n';
 			local_h += h;
 		}
+
+		std::cout << '\n' << error << '\n' << '\n';
+
+		if (k != 0)
+			std::cout << "Expected error ratio: " << 4 << '\n' << std::setprecision(10)
+			<< "Actual error ratio: " << prev_error / error << '\n' << '\n';
 
 		std::cout << '\n' << "Continue(y/n)?: ";
 		std::cin >> goon;
 
-		N *= 2;
+		k++;
+
 		h /= 2;
+
+		prev_error = error;
 	}
 }
