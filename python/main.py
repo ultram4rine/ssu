bd06816805b5675ec1d2b3b5cc9fc1f5aee6a9f4
ultram4rine <ultramarine730@gmail.com>
2019-11-30 22:24:02 +0400

    sqlite support

--- python/main.py
+++ python/main.py
@@ -1,8 +1,10 @@
-import datetime
-import csv
 from classes import Route, Driver, Work
+
 from csv_work import read_routes_from_csv, read_drivers_from_csv, read_works_from_csv, write_routes_to_csv, write_drivers_to_csv, write_works_to_csv
-from xml_work import read_drivers_from_xml, read_routes_from_xml, read_works_from_xml, write_routes_to_xml, write_drivers_to_xml, write_works_to_xml
+
+from xml_work import read_routes_from_xml, read_drivers_from_xml, read_works_from_xml, write_routes_to_xml, write_drivers_to_xml, write_works_to_xml
+
+from sqlite_work import read_routes_from_sqlite, read_drivers_from_sqlite, read_works_from_sqlite, write_routes_to_sqlite, write_drivers_to_sqlite, write_works_to_sqlite
 
 routesCSV = read_routes_from_csv('csv/routes.csv')
 for route in routesCSV:
@@ -40,3 +42,21 @@ write_routes_to_xml(routesXML, 'xml/out/routes.xml')
 write_drivers_to_xml(driversXML, 'xml/out/drivers.xml')
 
 write_works_to_xml(worksXML, 'xml/out/works.xml')
+
+write_routes_to_sqlite(routesXML)
+
+write_drivers_to_sqlite(driversXML)
+
+write_works_to_sqlite(worksXML)
+
+routesSQL = read_routes_from_sqlite()
+for route in routesSQL:
+    route.display_info()
+
+driversSQL = read_drivers_from_sqlite()
+for driver in driversSQL:
+    driver.display_info()
+
+worksSQL = read_works_from_sqlite()
+for work in worksSQL:
+    work.display_info()
