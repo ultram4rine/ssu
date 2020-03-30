76ff40d8625c9788de10723f7d5c05530409bc10
ultram4rine <ultramarine730@gmail.com>
2020-03-30 20:44:53 +0400

    ignore target

--- numerical/PDEs/target/debug/deps/pdes.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ultra\repos\github.com\ultram4rine\ssu\numerical\PDEs\target\debug\deps\pdes.exe: src\main.rs
-
-C:\Users\ultra\repos\github.com\ultram4rine\ssu\numerical\PDEs\target\debug\deps\pdes.d: src\main.rs
-
-src\main.rs:
--- numerical/PDEs/target/rls/debug/deps/pdes-b8e8930f9aaef9e4.d
+++
@@ -1,5 +0,0 @@
-c:\Users\ultra\repos\github.com\ultram4rine\ssu\numerical\PDEs\target\rls\debug\deps\pdes-b8e8930f9aaef9e4.rmeta: src\main.rs
-
-c:\Users\ultra\repos\github.com\ultram4rine\ssu\numerical\PDEs\target\rls\debug\deps\pdes-b8e8930f9aaef9e4.d: src\main.rs
-
-src\main.rs:
