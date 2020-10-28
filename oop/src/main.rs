8400d082ab817dc3042aa67e0102bed61f93662c
ultram4rine <ultramarine730@gmail.com>
2020-10-28 15:29:44 +0400

    sqlite connection

--- oop/src/main.rs
+++ oop/src/main.rs
@@ -1,10 +1,52 @@
+#[macro_use]
+extern crate serde_derive;
+
 mod models;
 
-fn main() {
-    let p = models::product::Product::new(1, "Gold Ring".to_string(), 600);
+use models::contract::Contract;
+use models::customer::Customer;
+use models::product::Product;
 
-    let cust =
-        models::customer::Customer::new(1, "Some name".to_string(), "89990000000".to_string());
+use rusqlite::{Connection, Result, NO_PARAMS};
 
-    println!("{}, {}", p.id, cust.id)
+fn main() -> Result<()> {
+    let conn = Connection::open("sqlite.db")?;
+
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS product (
+            id              INTEGER PRIMARY KEY,
+            name            TEXT NOT NULL,
+            cost            INTEGER
+        );
+        CREATE TABLE IF NOT EXISTS customer (
+            id              INTEGER PRIMARY KEY,
+            name            TEXT NOT NULL,
+            phone           TEXT NOT NULL
+        );
+        CREATE TABLE IF NOT EXISTS contracts (
+            id INT              INTEGER PRIMARY KEY,
+            customer_id         INT,
+            return_date         TEXT,
+            factual_return_date TEXT,
+            start_cost          INT,
+            buyback_cost        INT,
+            closed              INT,
+            FOREIGN KEY (customer_id) REFERENCES customers (id) ON DELETE NO ACTION ON UPDATE CASCADE
+        );
+        CREATE TABLE IF NOT EXISTS contracts_products (
+            contract_id INT,
+            product_id  INT,
+            FOREIGN KEY (contract_id) REFERENCES contracts (id) ON DELETE CASCADE ON UPDATE CASCADE,
+            FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE NO ACTION ON UPDATE CASCADE
+        );",
+        NO_PARAMS,
+    )?;
+
+    let p = Product::new(1, "Gold Ring".to_string(), 600);
+
+    let cust = Customer::new(1, "Some name".to_string(), "89990000000".to_string());
+
+    println!("{}, {}", p.id, cust.id);
+
+    Ok(())
 }
