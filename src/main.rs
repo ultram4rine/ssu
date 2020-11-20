06bf6caa40ec7ec66b06354011830610fa544b0b
ultram4rine <ultramarine730@gmail.com>
2020-11-20 14:50:18 +0400

    basically works

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .margin(5)
         .x_label_area_size(30)
         .y_label_area_size(30)
-        .build_cartesian_2d(-0.1f32..1.1f32, -0.1f32..1.1f32)?;
+        .build_cartesian_2d(0f32..1f32, 0f32..1f32)?;
 
     chart.configure_mesh().draw()?;
 
-    for n in 0..11 {
+    let colors = [&RED, &GREEN, &BLUE, &YELLOW, &MAGENTA, &CYAN];
+
+    for n in 12..13 {
+        let i = n % 6;
+        let color = &RED;
+
         chart
             .draw_series(LineSeries::new(
-                (0..=50).map(|x| x as f32 / 50.0).map(|x| (x, fi(x, n))),
+                (0..=500000)
+                    .map(|x| x as f32 / 500000.0)
+                    .map(|x| (x, fi(x, n))),
                 &RED,
             ))?
             .label(format!("fi{}(x)", n))
@@ -39,12 +46,10 @@ fn fi(x: f32, n: i32) -> f32 {
     } else {
         let (k, i) = count_k_and_i(n);
 
-        if x == (2_f32 * i - 1_f32) / (2_f32.powf(k + 1_f32)) {
-            return 1_f32;
-        } else if (x <= (i - 1_f32) / 2_f32.powf(k)) && (x >= i / 2_f32.powf(k)) {
-            return 0_f32;
+        if ((2_f32 * i - 2_f32) / 2_f32.powf(k) <= x) && (x <= 2_f32 * i / 2_f32.powf(k)) {
+            return 1_f32 - (x * 2_f32.powf(k) - 2_f32 * i + 1_f32).abs();
         } else {
-            return (1_f32 - (x * 2_f32.powf(k + 1_f32) - 2_f32 * i + 1_f32).abs()).abs();
+            return 0_f32;
         }
     }
 }
