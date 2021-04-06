2735f1ed27a91ec351e0681dc18e07665c288785
ultram4rine <ultramarine730@gmail.com>
2021-04-07 00:40:38 +0400

    3rd exercise db schema and page skeleton

---
+++ php/scripts/3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Calculator</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <td>Товар</td>
+            <td>Цена</td>
+            <td>Количество</td>
+            <td>Стоимость</td>
+        </tr>
+    </table>
+</body>
+
+</html>
