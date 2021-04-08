6ba09bcea115481f978f2636941009b3b8841a42
ultram4rine <ultramarine730@gmail.com>
2021-04-08 22:58:39 +0400

    do not show add buttons if not admin

--- php/site/header.php
+++ php/site/header.php
@@ -1,18 +1,23 @@
 <?php
     session_start();
-    if($_SESSION['user_id']){}
-    else{
-        header("location: login.php");
-    }
-    $user = $_SESSION['user_id'];
 
-    printf('
-    <header>
-        <a href="/gantt.php" class="logo">Распределение задач</a>
-        <div class="header-right">
+    $user = $_SESSION['user_id'];
+    $is_root = $_SESSION['user_is_root'];
+
+    print('
+        <header>
+            <a id="chart" class="logo" href="/gantt.php">Распределение задач</a>
+                <div class="header-right">');
+
+        if ($is_root) {
+        print('
             <a id="addtask" href="/addtask.php">Добавить задачу</a>
             <a id="adduser" href="/adduser.php">Добавить пользователя</a>
-            <a id="profile" href="/user.php?id=%s">Профиль</a>
-        </div>
-    </header>', $user);
+        ');
+    }
+
+    printf('
+                <a id="profile" href="/user.php?id=%s">Профиль</a>
+            </div>
+        </header>', $user);
 ?>
