0a43224f8764f458cae8fc74657f92ab7583fb23
ultram4rine <ultramarine730@gmail.com>
2021-04-10 22:09:35 +0400

    unique and cascade

--- php/site/schema.sql
+++ php/site/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     root BOOLEAN NOT NULL,
     full_name VARCHAR(100) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE tasks (
     user_id INT NOT NULL,
     planned_closed_at DATE NOT NULL,
     closed_at DATE NULL,
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL UNIQUE,
     description TEXT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
