159157a1cd5813e437e44f165507d6c4a8ca0b00
ultram4rine <ultramarine730@gmail.com>
2021-04-10 19:50:00 +0400

    added deletion scripts

---
+++ php/site/deleteuser.php
@@ -0,0 +1,19 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
+        $id = htmlspecialchars($_GET["id"]);
+        if (!empty($id)) {
+            require_once 'conn.php';
+
+            $mysqli = new mysqli($host, $user, $password, $database);
+            if ($mysqli->connect_errno) {
+                printf("Соединение не удалось: %s\n", $mysqli->connect_error);
+                exit();
+            }
+
+            $result = $mysqli->query("DELETE FROM users WHERE id = $id");
+
+            $mysqli->close();
+        }
+        header('Location: gantt.php');
+    }
+?>
