15c3e7257db1ff894a2ec97cdcec2e440d94922f
ultram4rine <ultramarine730@gmail.com>
2021-04-27 12:38:04 +0400

    deleted unused

--- numerical/sturm-liouville/src/methods/galerkin.rs
+++
@@ -1,3 +0,0 @@
-pub fn galerkin_method(q: fn(f64) -> f64) -> f64 {
-    0.0
-}
--- numerical/sturm-liouville/src/methods/shooting.rs
+++
@@ -1,5 +0,0 @@
-/// If Sturm–Liouville equation has Dirichlet boundary condition,
-/// we can extract IVP with conditions u(a) = 0, u'(a) = 1.
-pub fn shooting_method(q: fn(f64) -> f64) -> f64 {
-    0.0
-}
