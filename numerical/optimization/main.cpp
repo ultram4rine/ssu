35c3805c77de2e69a43970291bb63b7c239010a8
ultram4rine <ultramarine730@gmail.com>
2021-06-23 00:34:29 +0400

    rename

--- numerical/optimization/main.cpp
+++ numerical/optimization/main.cpp
@@ -21,43 +21,43 @@ int main()
      // initial point.
      array<double, 2> x_0 = {-1, 1};
 
-     array<double, 2> end_point1 = ccd(x_0, eps, f, golden_section_search);
-     array<double, 2> end_point2 = rosenbrock(x_0, eps, f);
-     array<double, 2> end_point3 = regular_simplex(x_0, eps, f);
-     array<double, 2> end_point4 = random_search(x_0, eps, f);
-     array<double, 2> end_point5 = pattern_search(x_0, eps, f);
+     array<double, 2> min1 = ccd(x_0, eps, f, golden_section_search);
+     array<double, 2> min2 = rosenbrock(x_0, eps, f);
+     array<double, 2> min3 = regular_simplex(x_0, eps, f);
+     array<double, 2> min4 = random_search(x_0, eps, f);
+     array<double, 2> min5 = pattern_search(x_0, eps, f);
 
      /*cout << "Value at initial point" << '\n';
      cout << f(x_0) << '\n';*/
 
      cout << "Coordinate Descent Method" << '\n'
           << '\n';
-     cout << "(" << end_point1[0] << ", " << end_point1[1] << ")" << '\n';
-     cout << f(end_point1) << '\n'
+     cout << "(" << min1[0] << ", " << min1[1] << ")" << '\n';
+     cout << f(min1) << '\n'
           << '\n';
 
      cout << "Rosenbrock Method" << '\n'
           << '\n';
-     cout << "(" << end_point2[0] << ", " << end_point2[1] << ")" << '\n';
-     cout << f(end_point2) << '\n'
+     cout << "(" << min2[0] << ", " << min2[1] << ")" << '\n';
+     cout << f(min2) << '\n'
           << '\n';
 
      cout << "Regular simplex method" << '\n'
           << '\n';
-     cout << "(" << end_point3[0] << ", " << end_point3[1] << ")" << '\n';
-     cout << f(end_point3) << '\n'
+     cout << "(" << min3[0] << ", " << min3[1] << ")" << '\n';
+     cout << f(min3) << '\n'
           << '\n';
 
      cout << "Random search method" << '\n'
           << '\n';
-     cout << "(" << end_point4[0] << ", " << end_point4[1] << ")" << '\n';
-     cout << f(end_point4) << '\n'
+     cout << "(" << min4[0] << ", " << min4[1] << ")" << '\n';
+     cout << f(min4) << '\n'
           << '\n';
 
      cout << "Pattern search method" << '\n'
           << '\n';
-     cout << "(" << end_point5[0] << ", " << end_point5[1] << ")" << '\n';
-     cout << f(end_point5) << '\n'
+     cout << "(" << min5[0] << ", " << min5[1] << ")" << '\n';
+     cout << f(min5) << '\n'
           << '\n';
 
      return 0;
