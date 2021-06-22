6ff4ea1cb8190d79f7b04a68ffe1369704cf6958
ultram4rine <ultramarine730@gmail.com>
2021-06-22 22:29:47 +0400

    done with random search

--- numerical/optimization/main.cpp
+++ numerical/optimization/main.cpp
@@ -5,6 +5,7 @@
 #include "ccd.hpp"
 #include "rosenbrock.hpp"
 #include "regular_simplex.hpp"
+#include "random_search.hpp"
 
 using namespace std;
 
@@ -19,29 +20,36 @@ int main()
      // initial point.
      array<double, 2> x_0 = {-1, 1};
 
-     array<double, 2> end_point3 = ccd(x_0, eps, f, golden_section_search);
-     array<double, 2> end_point4 = rosenbrock(x_0, eps, f);
-     array<double, 2> end_point5 = regular_simplex(x_0, eps, f);
+     array<double, 2> end_point1 = ccd(x_0, eps, f, golden_section_search);
+     array<double, 2> end_point2 = rosenbrock(x_0, eps, f);
+     array<double, 2> end_point3 = regular_simplex(x_0, eps, f);
+     array<double, 2> end_point4 = random_search(x_0, eps, f);
 
      /*cout << "Value at initial point" << '\n';
      cout << f(x_0) << '\n';*/
 
      cout << "Coordinate Descent Method" << '\n'
           << '\n';
-     cout << "(" << end_point3[0] << ", " << end_point3[1] << ")" << '\n';
-     cout << f(end_point3) << '\n'
+     cout << "(" << end_point1[0] << ", " << end_point1[1] << ")" << '\n';
+     cout << f(end_point1) << '\n'
           << '\n';
 
      cout << "Rosenbrock Method" << '\n'
           << '\n';
-     cout << "(" << end_point4[0] << ", " << end_point4[1] << ")" << '\n';
-     cout << f(end_point4) << '\n'
+     cout << "(" << end_point2[0] << ", " << end_point2[1] << ")" << '\n';
+     cout << f(end_point2) << '\n'
           << '\n';
 
      cout << "Regular simplex method" << '\n'
           << '\n';
-     cout << "(" << end_point5[0] << ", " << end_point5[1] << ")" << '\n';
-     cout << f(end_point5) << '\n'
+     cout << "(" << end_point3[0] << ", " << end_point3[1] << ")" << '\n';
+     cout << f(end_point3) << '\n'
+          << '\n';
+
+          cout << "Random search method" << '\n'
+          << '\n';
+     cout << "(" << end_point4[0] << ", " << end_point4[1] << ")" << '\n';
+     cout << f(end_point4) << '\n'
           << '\n';
 
      return 0;
--- numerical/optimization/random_search.cpp
+++ numerical/optimization/random_search.cpp
@@ -7,8 +7,49 @@
 
 #include "random_search.hpp"
 
+double fRand(double fMin, double fMax)
+{
+    double f = (double)rand() / RAND_MAX;
+    return fMin + f * (fMax - fMin);
+}
+
+double norm(array<double, 2> x)
+{
+    double sum = 0;
+    for (auto i = 0; i < 2; i++)
+    {
+        sum += x[i];
+    }
+    return sqrt(sum);
+}
+
 // Random search method.
 array<double, 2> random_search(array<double, 2> x_0, double eps, twoDFunc f)
 {
-    return {0, 0};
+    int j = 0, M = 100;
+    double alpha = 1, gamma = 2;
+    array<double, 2> x = x_0;
+
+    do
+    {
+        array<double, 2> xi = {fRand(-1, 1), fRand(-1, 1)};
+
+        array<double, 2> y = {x_0[0] + alpha * (xi[0] / norm(xi)), x_0[1] + alpha * (xi[1] / norm(xi))};
+
+        if (f(y) < f(x))
+        {
+            x = y;
+        }
+        else
+        {
+            j++;
+            if (j < M)
+            {
+                alpha /= gamma;
+                j = 0;
+            }
+        }
+    } while (alpha > eps);
+
+    return x;
 }
